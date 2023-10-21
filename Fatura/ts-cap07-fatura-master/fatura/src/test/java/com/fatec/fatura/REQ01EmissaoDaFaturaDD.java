b5b741a11650f6c3bca012b69a0251832d49cccb
Leonardo Rufino <119766870+leorufinx@users.noreply.github.com>
2023-10-20 22:07:54 -0300

    Aula02

---
+++ Fatura/ts-cap07-fatura-master/fatura/src/test/java/com/fatec/fatura/REQ01EmissaoDaFaturaDD.java
@@ -0,0 +1,56 @@
+package com.fatec.fatura;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.fatec.fatura.model.Fatura;
+
+class REQ01EmissaoDaFaturaDD {
+	Fatura fatura = new Fatura();
+
+	public String obtemDataAtual() {
+		DateTime data = new DateTime();
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/YYYY");
+		// DateTime dataVencimento = dataAtual.plusDays(10);
+		return data.toString(fmt);
+	}
+
+	String dataVenc = obtemDataAtual();
+
+	@ParameterizedTest
+	@CsvSource({
+			//classes de equivalencia pelo menos um valido e um invalido por atributo
+		"1,57734783000154, 20/12/2023, moveis planejados, 1500,satisfatorio",
+		"2,12345, 20/12/2023, moveis planejados, 1500,CNPJ invalido",
+		"3,57734783000154, 07/04/2023, moveis planejados, 1500,Data de vencimento invalida",
+		"4,57734783000154, 20/12/2023, moveis planejados, 0,Valor invalido",
+		"5,57734783000154, 20/12/2023,, 1500,Descricao do servico invalido",
+	
+			//analise do valor limite (selecione limites que sao relevantes para teste)
+			//teste funcional sistematico (2 ct por particao, obrigatorio branco, null e zero, 
+		    //caractere especial, limite de strings e de valores numericos
+			//analise de cobertura - todos os comandos
+			//analise de cobertura - todas as arestas
+	})
+
+	// é possivel criar um arquivo csv com a massa de dados no source folder de teste
+	// @CsvFileSource(resources = "\\fatura.csv", numLinesToSkip = 1)
+	void validaFatura(int numero, String cnpj, String dataVencimento, String desc, String valor, String re) {
+		try {
+			fatura = new Fatura(numero, cnpj, dataVencimento, desc, valor);
+			assertNotNull(fatura);
+			String dataDeHoje = obtemDataAtual();
+			assertTrue(dataDeHoje.equals(fatura.getDataEmissao()));
+			assertEquals ("satisfatorio", re);
+		} catch (Exception e) {
+			assertEquals(re, e.getMessage());
+		}
+	}
+
+}
